8bd9bc77c891ff670a6d34197a5596abe74dd190
sofia flores <saflores2@uc.cl>
2023-02-22 16:28:04 -0300

    ejercicio yelp

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
+  # get 'reviews/new'
+  # post 'restaurants/crate'
+  # get 'restaurants/show'
+  # get 'restaurants/new'
+  # get 'restaurants/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants, only: [:create, :show, :new, :index] do
+    # restaurantes/id/reviews/new
+    resources :reviews, only: [:create]
+  end
 end
---
+++ db/migrate/20230222153612_sample_name_change_column_type.rb
@@ -0,0 +1,5 @@
+class SampleNameChangeColumnType < ActiveRecord::Migration[7.0]
+  def change
+    change_column(:reviews, :rating, :integer)
+  end
+end
